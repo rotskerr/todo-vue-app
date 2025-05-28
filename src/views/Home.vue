546ce1fce2d9319880eaf3b93cef1ebaffff16bf
rotskerr <vvribakk@gmail.com>
2025-05-28 16:39:19 +0300

    feat: додано анімацію для задач через transition-group

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,15 @@
     <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Додати задачу" />
     <button @click="addTodo">Додати</button>
     <ul>
-      <TodoItem
-        v-for="(todo, index) in todoStore.todos"
-        :key="index"
-        :todo="todo"
-        @toggle="toggleTodo(index)"
-        @remove="removeTodo(index)"
-      />
+      <transition-group name="fade" tag="ul">
+        <TodoItem
+          v-for="(todo, index) in todoStore.todos"
+          :key="todo.text + index"
+          :todo="todo"
+          @toggle="toggleTodo(index)"
+          @remove="removeTodo(index)"
+        />
+      </transition-group>
     </ul>
   </div>
 </template>
@@ -34,6 +36,16 @@ function removeTodo(index) {
   todoStore.removeTodo(index)
 }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: all 0.4s;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+</style>
 
 <script>
 export default {
