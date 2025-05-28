6ab2f1ec116138a59fdba1f9a12669e0c891f04d
Владислав Войчишин <72383276+rotskerr@users.noreply.github.com>
2025-05-29 01:25:35 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPlWANqjZZ11B9UFHaMQOcGdiJPCA9LfA",
-  authDomain: "kanban-todo-app.firebaseapp.com",
-  projectId: "kanban-todo-app",
-  storageBucket: "kanban-todo-app.firebasestorage.app",
-  messagingSenderId: "165139419766",
-  appId: "1:165139419766:web:5384bd2f72cb0394a6c557",
-  measurementId: "G-701MX0TKJ1"
+
 };
 
 const app = initializeApp(firebaseConfig);
